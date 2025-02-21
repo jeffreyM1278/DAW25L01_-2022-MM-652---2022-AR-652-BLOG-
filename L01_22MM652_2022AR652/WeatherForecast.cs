d44598362c4aa87bc81141f6a14406170e771a85
jeffr <jeffr@LAPTOP-NS6Q6ARS>
2025-02-20 18:15:33 -0600

    Agregar archivos de proyecto.

---
+++ L01_22MM652_2022AR652/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace L01_22MM652_2022AR652
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
