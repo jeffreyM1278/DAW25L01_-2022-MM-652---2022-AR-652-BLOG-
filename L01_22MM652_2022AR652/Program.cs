69a1cd531d3ca5fd8d6d0853afdaa06890ce35a2
jeffr <jeffr@LAPTOP-NS6Q6ARS>
2025-02-20 19:10:37 -0600

    cambios

--- L01_22MM652_2022AR652/Program.cs
+++ L01_22MM652_2022AR652/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using L01_22MM652_2022AR652.Models; 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<blogDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("bibliotecaDBconnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
